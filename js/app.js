0bf5ddd5a22083a9460b1d341bb4881b6c7c5d33
Juno0w0 <juuzoujunior12@gmail.com>
2025-08-26 16:43:22 -0500

    JS implemetation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+/*LOGO con redireccionamiento*/
+const logo = document.getElementById('logo');
+logo.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+/*Año automatico en el footer*/
+const year = new Date().getFullYear();
+document.querySelector(".footer-text").innerHTML = `Desarrollado por Juno © ${year} CyberLearn. Todos los derechos reservados`;
+
+const yearLogin = new Date().getFullYear();
+document.querySelector(".footer-login").innerHTML = `Desarrollado por Juno © ${yearLogin} CyberLearn. Todos los derechos reservados`;
+
+/*Boton de busqueda*/
 const searchBtn = document.getElementById('searchBtn');
 const searchInput = document.getElementById('searchInput');
 
